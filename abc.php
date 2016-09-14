d2a6bf47c4cc6bf88f2fcdad4090c4d98e8ff95e
VANI MN <VANI MN>
2016-09-14 11:21:45 -0400

    Trying multi-line comment. This will be added as a paragraph. Use esc :wq to exit

--- abc.php
+++ abc.php
@@ -2,7 +2,7 @@ Modifying the test file
 Adding a brand new line
 Modifying the test file
 Adding a new line
-Modifying the test file
+Modifying the test file again
 Adding a brand new line
 Modifying the test file
 Adding a brand new line
